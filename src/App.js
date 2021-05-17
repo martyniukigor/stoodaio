01ae516b4d29c8e4edd644a0bf14b8b6e49be64f
martiniti <martiniti@ukr.net>
2021-05-17 17:47:38 +0300

    Revert "Basic project"
    This reverts commit f1a68f92

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import React from 'react';
-import {Route, Switch} from 'react-router-dom'
-
-//Components
-import {Pravi} from "./component/Pravi";
-
-//Pages & Blocks
-import {Home} from "./pages/Home";
-import {Login} from "./pages/Login";
-import {Profile} from "./pages/Profile";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-      <Route render={({location}) => (
-
-          <main>
-
-            <Pravi />
-
-                <Switch location={location}>
-
-                  <Route path='/' exact component={Home} />
-
-                  <Route path='/login' component={Login} />
-
-                  <Route path='/profile' component={Profile} />
-
-                </Switch>
-
-          </main>
-
-      )} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
